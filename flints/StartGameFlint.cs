0ac269d96013fc76e67f98ee1ca1c729a78be45e
MessCoder <messcoderewe@gmail.com>
2016-06-02 19:20:41 +0200

    Reorganized code, minor code comments

---
+++ flints/StartGameFlint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.october.flints
+{
+    /// <summary>
+    /// Initializes a game session in a game map scene
+    /// </summary>
+    public class StartGameFlint : Flint
+    {
+        // Number of players on preview execution
+        [SerializeField]
+        private int numPlayers = 1;
+        
+        protected override void NormalSetup()
+        {
+            // This initialization is performed by the GameManager
+            // in normal executions
+            //
+            // This class only exists in order to make preview plays 
+            // possible
+        }
+
+        protected override void PreviewSetup()
+        {
+            IngameManager.startGame(numPlayers);
+        }
+    }
+}
